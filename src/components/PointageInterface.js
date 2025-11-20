29c73e3a6cb0c263408391ed4a08f226d601b152
Yvesco75 <yvesgodonou4@gmail.com>
2025-11-20 16:26:30 +0100

    20/11/2025..

--- src/components/PointageInterface.js
+++ src/components/PointageInterface.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './PointageInterface.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://biblio-backend-5b8g.onrender.com/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 function PointageInterface() {
   const [telephone, setTelephone] = useState('');
